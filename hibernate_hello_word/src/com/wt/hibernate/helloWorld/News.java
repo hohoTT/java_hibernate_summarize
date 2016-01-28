52ca00fe5292dfc841a694674f8530324d830f40
hohoTT <609029365@qq.com>
2016-01-28 20:39:38 +0800

    第一个使用hibernate创建数据库表，并添加数据的例子。

---
+++ hibernate_hello_word/src/com/wt/hibernate/helloWorld/News.java
@@ -0,0 +1,61 @@
+package com.wt.hibernate.helloWorld;
+
+import java.sql.Date;
+
+public class News {
+	
+	private Integer id;
+	private String title;
+	private String author;
+	
+	private Date date;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+	public News(String title, String author, Date date) {
+		super();
+		this.title = title;
+		this.author = author;
+		this.date = date;
+	}
+	
+	public News() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public String toString() {
+		return "News [id=" + id + ", title=" + title + ", author=" + author
+				+ ", date=" + date + "]";
+	}
+}
