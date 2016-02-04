89d207eb92cc93b9b9431583ebf751b19d6198c5
hohoTT <609029365@qq.com>
2016-02-04 16:13:06 +0800

    添加 hibernate 映射文件中 set 的三个属性（inverse、cascade、order-by）的描述及使用说明

--- hibernate_mapping/src/com/wt/hibernate/n_to_1/both/HibernateText.java
+++ hibernate_mapping/src/com/wt/hibernate/n_to_1/both/HibernateText.java
@@ -9,6 +9,7 @@ import java.sql.Blob;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.Date;
+import java.util.Set;
 
 import org.hibernate.Hibernate;
 import org.hibernate.Session;
@@ -55,7 +56,11 @@ public class HibernateText {
 		sessionFactory.close();
 	}
 	
-	
+	@Test
+	public void testCasecade() {
+		Customer customer = (Customer) session.get(Customer.class, 2);
+		customer.getOrders().clear();
+	}
 	
 	@Test
 	public void testDelete() {
@@ -79,15 +84,24 @@ public class HibernateText {
 	@Test
 	public void testOneToManyGet() {
 		// 1. 对多的一端的集合使用延迟加载
-		Customer customer = (Customer) session.get(Customer.class, 1);
+		Customer customer = (Customer) session.get(Customer.class, 5);
 		System.out.println(customer.getCustomerName());
 		
 		// 2. 返回多的一端的集合是 Hibernate 内置的集合类型
 		// 该类具有延迟加载和存放代理对象的功能
 		System.out.println(customer.getOrders().getClass());
 		
-		// 3. 可能会跑出 LazyInitializationException 异常
-//		System.out.println(customer.getOrders().size());
+		// 3. 可能会抛出 LazyInitializationException 异常
+		System.out.println(customer.getOrders().size());
+		
+		// 检验查询的结果是否为根据映射文件中通过order-by="ORDER_NAME DESC"
+		// 对 order 按照ORDER_NAME进行降序排列
+		Set<Order> orders = customer.getOrders();
+		
+		for (Order order : orders) {
+			System.out.println("--------------");
+			System.out.println(order.getOrderName());
+		}
 		
 		// 4. 在需要使用集合中元素的时候进行初始化
 	}
@@ -114,13 +128,13 @@ public class HibernateText {
 	@Test
 	public void testManyToOneBothSave() {
 		Customer customer = new Customer();
-		customer.setCustomerName("CC");
+		customer.setCustomerName("AA");
 		
 		Order order1 = new Order(); 
-		order1.setOrderName("ORDER-5");
+		order1.setOrderName("ORDER-1");
 		
 		Order order2 = new Order();
-		order2.setOrderName("ORDER-6");
+		order2.setOrderName("ORDER-2");
 
 		// 设定关联关系
 		order1.setCustomer(customer);
@@ -137,8 +151,8 @@ public class HibernateText {
 		// 好处是不会多出 update 语句
 		session.save(customer);
 		
-		session.save(order1);
-		session.save(order2);	
+//		session.save(order1);
+//		session.save(order2);	
 
 		// 先插入 Order，再插入 Customer，
 		// 结果为执行三条 insert，四条 update
